ccfded3f1ee43474783fc571e3e5f312ee5a9fa5
asannier <andrew_sannier@cable.comcast.com>
2014-11-26 13:53:57 -0700

    Initial commit; working first draft

---
+++ gitPullLoop.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"os/exec"
+)
+
+func main() {
+	wd, _ := os.Getwd()
+	dirs, _ := ioutil.ReadDir(wd)
+	didAnything := false
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			os.Chdir(dir.Name())
+			if isRepo("./") {
+				fmt.Println(dir.Name())
+				go gitPull() 	// Run the long-taking part on its own thread
+				didAnything = true
+			}
+			os.Chdir("../")
+		}
+	}
+	if !didAnything {
+		fmt.Println("No git repositories found")
+	}
+}
+
+// Perform a git pull in the current working directory
+// (assumes that git is on your $PATH)
+func gitPull() {
+	cmd := exec.Command("git", "pull")
+	err := cmd.Run()
+	if nil != err {
+		fmt.Println(err)
+		panic(err)
+	}	
+}
+
+// Given a relative path as a string, determine if the directory contains
+// a .git directory
+func isRepo(dir string) bool {
+	files, _ := ioutil.ReadDir(dir)
+	for _, file := range files {
+		if  file.Name() == ".git" {
+			return true
+		}
+	}
+	return false
+}
