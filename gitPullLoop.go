73ba6bb2c1619a08130ff2de4a7edd387d431fdf
asannier <andrew_sannier@cable.comcast.com>
2014-12-29 11:56:05 -0700

    Still not quite working as goroutine, but close

--- gitPullLoop.go
+++ gitPullLoop.go
@@ -5,23 +5,33 @@ import (
 	"os"
 	"io/ioutil"
 	"os/exec"
+	"sync"
 )
 
 func main() {
 	wd, _ := os.Getwd()
 	dirs, _ := ioutil.ReadDir(wd)
 	didAnything := false
+	wg := new(sync.WaitGroup)
+	wg.Add(len(dirs))
 	for _, dir := range dirs {
 		if dir.IsDir() {
-			os.Chdir(dir.Name())
-			if isRepo("./") {
-				fmt.Println(dir.Name())
-				go gitPull() 	// Run the long-taking part on its own thread
-				didAnything = true
+			if isRepo(dir.Name()) {
+				func(dirName string, wg *sync.WaitGroup) {
+					gitPull(dirName)
+					defer func() {
+						didAnything = true
+						wg.Done()
+					}()
+				}(dir.Name(), wg)
+			} else {
+				wg.Done()
 			}
-			os.Chdir("../")
+		} else {
+			wg.Done()
 		}
-	}
+	}	
+	wg.Wait()
 	if !didAnything {
 		fmt.Println("No git repositories found")
 	}
@@ -29,13 +39,15 @@ func main() {
 
 // Perform a git pull in the current working directory
 // (assumes that git is on your $PATH)
-func gitPull() {
-	cmd := exec.Command("git", "pull")
+func gitPull(dir string) {
+	cmd := exec.Command("git", "-C", dir, "pull")
+	fmt.Println("Executing ", cmd.Args, "...")
 	err := cmd.Run()
 	if nil != err {
-		fmt.Println(err)
-		panic(err)
-	}	
+		fmt.Println("Failed to pull from ", dir)
+		return
+	}
+	fmt.Println("Finished pull from ", dir)
 }
 
 // Given a relative path as a string, determine if the directory contains
@@ -49,3 +61,19 @@ func isRepo(dir string) bool {
 	}
 	return false
 }
+
+// Starting from a string directory, recurse to find all directories
+// containing .git directories (i.e., find all git repos)
+// func traverseAndDo(dir string) []string {
+// 	files, _ := ioutil.ReadDir(dir)
+// 	var subdirs []string
+// 	for _, file := range files {
+// 		if file.IsDir && !isRepo(file) {
+// 			append(subdirs, file.Name())
+// 		}
+// 	}
+
+// 	for _, subdir := range subdirs {
+// 		traverse(subdir)
+// 	}
+// }
